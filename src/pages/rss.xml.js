3df1788e9db6489d5322cfb6e20a6d78c43e6416
Md saud ali <111742012+saudsync@users.noreply.github.com>
2026-02-12 16:36:34 +0530

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
-    title: 'Nagpur Mail',
-    description: 'Latest News and Updates from Nagpur.',
+    title: 'Nagpur Mail | Real-Time Updates',
+    description: 'Latest news and updates from Nagpur and Vidarbha region.',
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
@@ -15,4 +15,3 @@ export async function GET(context) {
     })),
   });
 }
-
